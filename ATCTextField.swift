177ce5b3cf1ca9002638d6d5a612d5c2646a0fe8
Florian Marcu <florian.mrc@gmail.com>
2018-08-16 21:12:39 -0700

    ATCTextField with padding

---
+++ ATCTextField.swift
@@ -0,0 +1,26 @@
+//
+//  ATCTextField.swift
+//  DashboardApp
+//
+//  Created by Florian Marcu on 8/9/18.
+//  Copyright © 2018 Instamobile. All rights reserved.
+//
+
+import UIKit
+
+class ATCTextField: UITextField {
+
+    let padding = UIEdgeInsets(top: 0, left: 15, bottom: 0, right: 5)
+
+    override open func textRect(forBounds bounds: CGRect) -> CGRect {
+        return UIEdgeInsetsInsetRect(bounds, padding)
+    }
+
+    override open func placeholderRect(forBounds bounds: CGRect) -> CGRect {
+        return UIEdgeInsetsInsetRect(bounds, padding)
+    }
+
+    override open func editingRect(forBounds bounds: CGRect) -> CGRect {
+        return UIEdgeInsetsInsetRect(bounds, padding)
+    }
+}
